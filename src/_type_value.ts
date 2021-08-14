4a406b75aa415cc59e59d75a5aba10c1e00d0b93
Phạm Linh <pnlinh.it@gmail.com>
2021-08-15 00:45:15 +0700

    Update typeof

--- src/_type_value.ts
+++ src/_type_value.ts
@@ -16,10 +16,23 @@ const user = new MyUser('linh', 1)
 // Pass type to generic
 type UpadteUser = Partial<MyUser>
 
+build1(MyUser)
+build2(MyUser)
+build3(user)
+
 export type AnyClass<ReturnType = any> = new (...args: any[]) => ReturnType;
 
-function build(user: AnyClass<MyUser>) {
-    return new user()
+function build1(user: AnyClass<MyUser>) {
+    return new user('linh', 12)
+}
+
+// https://www.typescriptlang.org/docs/handbook/2/typeof-types.html
+function build2(user: typeof MyUser) {
+    return new user('Linh', 12)
+}
+
+function build3(user: MyUser) {
+    return user
 }
 
 export default {}
